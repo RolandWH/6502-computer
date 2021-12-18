965a00c5a1cae1402df7b970ee42383baa9995b8
Roland W-H <r.weirhowell@gmail.com>
2021-12-18 11:28:23 +0000

    monitor: changed pin numbers and output format

--- 6502-monitor/6502-monitor.ino
+++ 6502-monitor/6502-monitor.ino
@@ -1,20 +1,15 @@
-const char ADDR[] = { 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52 };
-const char DATA[] = { 39, 41, 43, 45, 47, 49, 51, 53 };
 #define CLOCK 2
-#define RW 3
+#define RW 39
+
+const char ADDR[] = { 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52 };
+const char DATA[] = { 23, 25, 27, 29, 31, 33, 35, 37 };
 
 
 void setup()
 {
     // put your setup code here, to run once:
-    for (int i = 0; i < 16; i++)
-    {
-        pinMode(ADDR[i], INPUT);
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        pinMode(DATA[i], INPUT);
-    }
+    for (byte i = 0; i < 16; i++) pinMode(ADDR[i], INPUT);
+    for (byte i = 0; i < 8; i++) pinMode(DATA[i], INPUT);
     pinMode(CLOCK, INPUT);
     pinMode(RW, INPUT);
     Serial.begin(57600);
@@ -26,32 +21,32 @@ void setup()
 // Only print address and data values on clock pulse
 void OnClock()
 {
-    char output[15];
-    
+    char addrStr[16];
     unsigned int address = 0;
-    for (int i = 0; i < 16; i++)
+    for (byte i = 0; i < 16; i++)
     {
         int bit = digitalRead(ADDR[i]);
         Serial.print(bit);
         address = (address << 1) + bit;
     }
-    
-    Serial.print("   ");
-    
+    sprintf(addrStr, "  %04x    %c    ", address, digitalRead(RW) ? 'R' : 'W');
+    Serial.print(addrStr);
+
+    char dataStr[5];
     unsigned int data = 0;
-    for (int i = 0; i < 8; i++)
+    for (byte i = 0; i < 8; i++)
     {
         int bit = digitalRead(DATA[i]);
         Serial.print(bit);
         data = (data << 1) + bit;
     }
-
-    sprintf(output, "   %04x  %c %02x", address, digitalRead(RW) ? 'R' : 'W', data);
-    Serial.println(output);
+    sprintf(dataStr, "  %02x", data);
+    Serial.println(dataStr);
 }
 
 
-void loop() {
+void loop()
+{
     // put your main code here, to run repeatedly:
     
 }
